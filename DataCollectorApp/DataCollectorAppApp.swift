58c3f80730ddaeca36e130b0bbdbf225ad90c201
tbahrit.m <m.tbahriti@esi-sba.dz>
2025-02-05 10:16:38 +0100

    Initial Commit

---
+++ DataCollectorApp/DataCollectorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataCollectorAppApp.swift
+//  DataCollectorApp
+//
+//  Created by Mohammed TBAHRITI on 05/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct DataCollectorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DataCollectorAppTests/DataCollectorAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DataCollectorAppTests.swift
+//  DataCollectorAppTests
+//
+//  Created by Mohammed TBAHRITI on 05/02/2025.
+//
+
+import Testing
+@testable import DataCollectorApp
+
+struct DataCollectorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
